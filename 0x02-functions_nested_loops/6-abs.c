0673cafd956de57f44981a0a4393b13df5886828
Mikiyas Aklilu <86475944+MYKICROWN@users.noreply.github.com>
2022-04-12 22:41:41 +0300

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,33 +1,17 @@
 #include "main.h"
 
-
-
 /**
+ * _abs - COmputes the absolute value of an integer
  *
- *  * _abs - COmputes the absolute value of an integer
+ *@a: The integer to be computed
  *
- *   *
- *
- *    *@a: The integer to be computed
- *
- *     *
- *
- *      * Return: Absolute value
- *
- *       */
-
-
+ * Return: Absolute value
+ */
 
 int _abs(int a)
-
 {
-
-		if (a >= 0)
-
-					return (a);
-
-			else
-
-						return (a * -1);
-
+	if (a >= 0)
+		return (a);
+	else
+		return (a * -1);
 }
