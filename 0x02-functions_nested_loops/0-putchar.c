4d58d3b00e49373b1694b30be548050dbfac9a19
Mikiyas Aklilu <86475944+MYKICROWN@users.noreply.github.com>
2022-04-12 21:48:16 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,18 @@
 #include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	_putchar('_putchar');
+	char *pointer = "_putchar";
+
+	while (*pointer)
+	{
+		_putchar(*pointer);
+		pointer++;
+	}
 	_putchar('\n');
 
 	return (0);
