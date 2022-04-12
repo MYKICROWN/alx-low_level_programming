d20e3c2acba6ddb71b7229483f8eb6d62aec6ea8
Mikiyas Aklilu <86475944+MYKICROWN@users.noreply.github.com>
2022-04-12 22:39:11 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,33 +1,17 @@
 #include "main.h"
 
-
-
 /**
+ * _islower - Checks for lowercase character
  *
- *  * _islower - Checks for lowercase character
+ * @c: Number to be checked
  *
- *   *
- *
- *    * @c: Number to be checked
- *
- *     *
- *
- *      * Return: 1 (Success) OR 0 (Failure)
- *
- *       */
-
-
+ * Return: 1 (Success) OR 0 (Failure)
+ */
 
 int _islower(int c)
-
 {
-
-		if (c >= 97 && c <= 122)
-
-					return (1);
-
-			else
-
-						return (0);
-
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
 }
